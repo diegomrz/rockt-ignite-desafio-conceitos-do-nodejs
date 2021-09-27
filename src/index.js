2f05a014ef95c46ef65e45526ea7797fd42bda8c
diegomrz <diegomrz@outlook.com>
2021-09-27 01:18:00 -0300

    Adding POST router for Users and Todos

--- src/index.js
+++ src/index.js
@@ -1,29 +1,65 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if(!user){
+    return response.status(400).json({error: 'User does not exists!'});
+  }
+  request.username = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { username, name } = request.body;
+  const userAlreadyExists = users.some(
+    (user) => user.username === username)
+
+  if(userAlreadyExists)
+    return response.status(400).json({error: 'User already exists!'})
+
+  users.push({
+    id: uuidv4(),
+    todos: [{}],
+    username,
+    name
+  });
+
+  return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+  console.log(user);console.log(user);console.log(user);console.log(user);
+  
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false, 
+    deadline: new Date(deadline), 
+    created_at: new Date()
+  }
+  
+  user.push(todo);
+
+  return response.status(200).json(users);
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
